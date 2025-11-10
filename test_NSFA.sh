051906533409db0c4b015fa229f6e7a4afa4425b
thuzjc <87356963+thuzjc@users.noreply.github.com>
2025-11-10 15:47:53 +0800

    Update shell file

--- test_NSFA.sh
+++ test_NSFA.sh
@@ -1,5 +1,5 @@
-python test_Kaggle.py --data_dir ../Kaggle_new_split --ckpt_dir ../NSFA_ckpt --patient_id 1 --device_number 0
-python test_Kaggle.py --data_dir ../Kaggle_new_split --ckpt_dir ../NSFA_ckpt --patient_id 2 --device_number 0
-python test_Kaggle.py --data_dir ../Kaggle_new_split --ckpt_dir ../NSFA_ckpt --patient_id 3 --device_number 0
-python test_Kaggle.py --data_dir ../Kaggle_new_split --ckpt_dir ../NSFA_ckpt --patient_id 4 --device_number 0
-python test_Kaggle.py --data_dir ../Kaggle_new_split --ckpt_dir ../NSFA_ckpt --patient_id 5 --device_number 0
+python test_Kaggle.py --data_dir ../exp_30/Kaggle_dataset_30  --ckpt_dir ../exp_30/NSFA_ckpt_30 --patient_id 1 --device_number 0 --target_preictal_interval 30
+python test_Kaggle.py --data_dir ../exp_30/Kaggle_dataset_30  --ckpt_dir ../exp_30/NSFA_ckpt_30 --patient_id 2 --device_number 0 --target_preictal_interval 30
+python test_Kaggle.py --data_dir ../exp_30/Kaggle_dataset_30  --ckpt_dir ../exp_30/NSFA_ckpt_30 --patient_id 3 --device_number 0 --target_preictal_interval 30
+python test_Kaggle.py --data_dir ../exp_30/Kaggle_dataset_30  --ckpt_dir ../exp_30/NSFA_ckpt_30 --patient_id 4 --device_number 0 --target_preictal_interval 30
+python test_Kaggle.py --data_dir ../exp_30/Kaggle_dataset_30  --ckpt_dir ../exp_30/NSFA_ckpt_30 --patient_id 5 --device_number 0 --target_preictal_interval 30
